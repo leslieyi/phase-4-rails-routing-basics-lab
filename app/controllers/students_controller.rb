1ccce8002f707c6a50d8cd7c28377207d9233682
leslieyi <leslieyi411@gmail.com>
2021-08-29 19:47:49 -0400

    commit tree b9d3d9728f8c47c756d5cbda9f40036d984783b3

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+    def index 
+        students = Student.all 
+        render json: students
+    end
+
+    def grades 
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+
+    def highest_grade 
+        student = Student.order(grade: :desc).first
+        # student = Student.all.max_by { |student| student.grade }
+        render json: student
+    end
+end
